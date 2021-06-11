db5259286d27a4e74f5a91fe59d39a804ff84044
Maiko Kieth Isaac Kitchon <maiko.kitchon@gmail.com>
2021-06-11 14:33:54 -0500

    Updated ::Login test for response in slack

--- selenium.py
+++ selenium.py
@@ -3,7 +3,7 @@ import requests
 import os
 import sys
 sys.path.insert(0, '.' + os.path.dirname(os.path.abspath(__file__)))
-from login import Login
+from lib.login import Login
 
 class Selenium(BotPlugin):
     """
